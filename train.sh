e3d86413d940c12908b52590eaee7dc1431f8ce7
I-Sheng Fang <nf0126@gmail.com>
2020-11-14 19:21:56 +0800

    for merge

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-python train.py --epochs 2 --generate_exp_name --pretrained --model_name resnet18 --radam
+python train.py --exp_name=''  --generate_exp_name --pretrained --model_name densenet121 --batch_size 8 --radam --random_apply_aug --random_horizontal_flip --random_rotation --random_erasing --random_order
+python train.py --exp_name=''  --generate_exp_name --pretrained --model_name densenet121 --batch_size 8 --radam --random_apply_aug --random_horizontal_flip --random_rotation --random_order
+python train.py --exp_name=''  --generate_exp_name --pretrained --model_name densenet121 --batch_size 8 --radam --random_apply_aug --random_horizontal_flip --random_order
